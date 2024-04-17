7639a8e6aa5c5d903790af57b298f20b05af938a
LuisFelipeAgit config --global user.name LuisFelipeAgit config --global user.name LuisFelipeA <felipeadriani2@gmail.com>
2024-04-17 07:42:36 -0300

    Arrays

---
+++ js/array.js
@@ -0,0 +1,21 @@
+var arr = [];
+
+console.log(arr);
+
+var arr = [5, "Luis", {idade:26, profissao:"Programador"}, true];
+
+console.log(arr);
+
+console.log(arr[1]);
+
+console.log(arr[2]);
+
+arr[4] = 22
+
+console.log(arr);
+
+arr[0] = 10;
+
+console.log(arr);
+
+console.log(typeof arr);
