36985f97370fcf035cfb8c4222ace6c879e84439
LuisFelipeAgit config --global user.name LuisFelipeAgit config --global user.name LuisFelipeA <felipeadriani2@gmail.com>
2024-04-18 07:43:01 -0300

    While

---
+++ js/estruturasDeRepeticao.js
@@ -0,0 +1,14 @@
+var i = 0;
+
+while (i < 3) {
+    console.log("Contador: " + i);
+    i++;
+};
+
+var y = 0
+var nomes = ['Luis', 'Felipe','José', 'Maria'];
+
+while (y < 3) {
+    console.log("Nome: " + nomes[y]);
+    y++;
+};
