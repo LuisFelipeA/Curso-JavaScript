d20690cdf532d6b77f113d3605fd459fc2d4b4cc
LuisFelipeAgit config --global user.name LuisFelipeAgit config --global user.name LuisFelipeA <felipeadriani2@gmail.com>
2024-04-23 07:36:55 -0300

    Metodos Numericos

---
+++ js/metodosNumericos.js
@@ -0,0 +1,28 @@
+// parseFloat
+
+console.log(parseFloat('12.999'));
+console.log(Number.parseFloat('12.999'));
+
+
+// parseInt
+
+console.log(parseInt('10'));
+console.log(parseInt(16.999));
+
+
+// toFixed
+
+console.log(23.165216565.toFixed(1));
+
+
+// isNaN
+
+console.log(isNaN('teste'));
+console.log(isNaN(12));
+console.log(isNaN('15'));
+
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
