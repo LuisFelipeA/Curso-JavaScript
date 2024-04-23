1fe078ee6159c64a2f76d7acd6df755f7b4a1972
LuisFelipeAgit config --global user.name LuisFelipeAgit config --global user.name LuisFelipeA <felipeadriani2@gmail.com>
2024-04-23 12:38:40 -0300

    Metodos String

--- js/metodosString.js
+++ js/metodosString.js
@@ -1,28 +1,48 @@
 // length
 
 var nome = "Luis";
-
 console.log(nome.length);
 
 
 // indexOf
 
 console.log(nome[2]);
-
 var frase = 'O rato roeu a roupa do rei de Roma';
-
 console.log(frase.indexOf('roeu'));
 
 
 // slice
 
 var roeu = frase.slice(7,11);
-
 console.log(roeu);
 
 
 // replace
 
 var novaFrase = frase.replace('roeu', 'teste');
-
 console.log(novaFrase);
+
+
+// yoLowerCase e toUppertCase
+
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+
+// Trim
+
+var nome = '            Luis';
+console.log(nome);
+console.log(nome.trim());
+
+
+// split
+
+var tags = 'PHP, JavaScript, HTML, CSS';
+console.log(tags.split(', '));
+
+
+// lastIndexOf
+
+var frase2 = 'Eu quero a ultima palavra teste dessa frase de teste';
+console.log(frase2.lastIndexOf('teste'));
