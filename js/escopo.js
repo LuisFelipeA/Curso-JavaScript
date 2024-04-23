713301fdde09177830c2446f483c0da3db84c522
LuisFelipeAgit config --global user.name LuisFelipeAgit config --global user.name LuisFelipeA <felipeadriani2@gmail.com>
2024-04-23 07:20:48 -0300

    escopo

---
+++ js/escopo.js
@@ -0,0 +1,24 @@
+var x = 1;
+var y = 2;
+
+console.log(x,y);
+
+function teste1() {
+    var z = 0;
+
+    console.log(z);
+
+    console.log(x,y);
+}
+
+//console.log(z);  //Variavel z esta declarada dentro da função
+
+teste1();
+
+function teste2() {
+    var z = 5
+
+    console.log(z);
+}
+
+teste2();
