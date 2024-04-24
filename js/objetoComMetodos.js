24c9ec461f1032671a511e84e00a4c412e16794b
LuisFelipeAgit config --global user.name LuisFelipeAgit config --global user.name LuisFelipeA <felipeadriani2@gmail.com>
2024-04-24 12:40:11 -0300

    Objeto com Metodos

---
+++ js/objetoComMetodos.js
@@ -0,0 +1,19 @@
+let pessoa = {
+    nome: "Luis",
+    idade: 29,
+    falar: function(){
+        console.log('Ola, tudo bem?')
+    },
+    soma: function(a, b) {
+        return a + b;
+    }
+};
+
+
+console.log(pessoa.nome);
+
+pessoa.falar();
+
+var soma = pessoa.soma(2,3);
+
+console.log(soma);
